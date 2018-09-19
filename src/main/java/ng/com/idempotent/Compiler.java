f93b4b74ffc9b0a140c8d754defd8afe9de18fba
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2018-09-19 19:23:15 +0100

    Force classes to define a serialVersionUID and make sure it's of type long.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/main/java/ng/com/idempotent/Compiler.java
+++ src/main/java/ng/com/idempotent/Compiler.java
@@ -1,9 +1,10 @@
 package ng.com.idempotent;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.ObjectOutputStream;
+import java.io.InvalidClassException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -23,9 +24,9 @@ public final class Compiler {
      * /home/dev/src/java/com/test/sjb/HelloWorld.java
      * @param fullyQualifiedClassName the fully qualified name of the class e.g
      * com.test.sjb.HelloWorld
-     * @return a HashMap containing the base64 encoded string of the
-     * instance of the class and a json docs of the name of the class, methods
-     * and parameters. The returned String will be uploaded to the blockchain.
+     * @return a HashMap containing the base64 encoded string of the instance of
+     * the class and a json docs of the name of the class, methods and
+     * parameters. The returned String will be uploaded to the blockchain.
      * @throws Exception
      */
     public static final HashMap<String, Object> compile(File source, String fullyQualifiedClassName) throws Exception {
@@ -38,25 +39,62 @@ public final class Compiler {
      * @param sourceCode a String containing the source code
      * @param fullyQualifiedClassName the fully qualified name of the class e.g
      * com.test.sjb.HelloWorld
-     * @return a HashMap containing the base64 encoded string of the
-     * instance of the class and a json docs of the name of the class, methods
-     * and parameters. The returned String will be uploaded to the blockchain.
+     * @return a HashMap containing the base64 encoded string of the instance of
+     * the class and a json docs of the name of the class, methods and
+     * parameters. The returned String will be uploaded to the blockchain.
      * @throws Exception
      */
     public static final HashMap<String, Object> compile(String sourceCode, String fullyQualifiedClassName) throws Exception {
         InMemoryJavaCompiler compiler = InMemoryJavaCompiler.newInstance();
         Class<?> compiled = compiler.compile(fullyQualifiedClassName, sourceCode);
+
+        Class[] interfaces = compiled.getInterfaces();
+        boolean hasSmartBean = false;
+        for (Class c : interfaces) {
+            if (c.getName().contains("SmartBean")) {
+                hasSmartBean = true;
+                break;
+            }
+        }
+
+        if (!hasSmartBean) {
+            throw new InvalidClassException(fullyQualifiedClassName + " does not implement the SmartBean Interface. All Smart Classes must implement the smart bean interface");
+        }
+
+        Field f = compiled.getDeclaredField("serialVersionUID");
+        System.out.println("Found Field: [" + f +"]");
+        if (f == null) {
+            throw new InvalidClassException(fullyQualifiedClassName + " does not have a variable called serialVersionUID. All Smart Classes must implement have this variable");
+        }
         
+        try {
+            int foundMods = f.getModifiers();
+            
+            if(!Modifier.isFinal(foundMods)) {
+                throw new InvalidClassException(fullyQualifiedClassName + " serialVersionUID does not have a final modifier. Please make serialVersionUID static final");
+            }
+            
+            if(!Modifier.isStatic(foundMods)) {
+                throw new InvalidClassException(fullyQualifiedClassName + " serialVersionUID does not have a static modifier. Please make serialVersionUID static final");
+            }
+            
+            if(!f.getType().getName().equals("long")) {
+                throw new InvalidClassException(fullyQualifiedClassName + " serialVersionUID must be of type long");
+            }            
+        } catch(Exception e) {
+            throw new InvalidClassException(e.getMessage());
+        }
+
         CompiledCode cc = compiler.classLoader.customCompiledCode.get(fullyQualifiedClassName);
-        byte[] byteCode = cc.getByteCode();        
-        
+        byte[] byteCode = cc.getByteCode();
+
         HashMap<String, Object> docsMap = new HashMap<>();
         docsMap.put("name", fullyQualifiedClassName);
 
         List<HashMap<String, Object>> methodsList = new ArrayList<>();
         Method[] methods = compiled.getMethods();
 
-        for (Method m : methods) {            
+        for (Method m : methods) {
             String declaringClass = m.getDeclaringClass().getName();
             if (declaringClass.equals(fullyQualifiedClassName)) {
                 HashMap<String, Object> methodsMap = new HashMap<>();
@@ -64,25 +102,23 @@ public final class Compiler {
                 methodsMap.put("name", name);
 
                 Parameter[] parameters = m.getParameters();
-                List<HashMap<String, String>> parametersList = new ArrayList<>();
+                List<String> parametersList = new ArrayList<>();
 
                 for (Parameter p : parameters) {
-                    HashMap<String, String> parametersMap = new HashMap<>();
-                    parametersMap.put("type", p.getType().getCanonicalName());
-                    parametersList.add(parametersMap);
+                    parametersList.add(p.getType().getCanonicalName());
                 }
                 methodsMap.put("parameters", parametersList);
                 methodsMap.put("returnType", m.getReturnType().getCanonicalName());
                 methodsList.add(methodsMap);
             }
         }
-        
+
         docsMap.put("methods", methodsList);
 
         HashMap<String, Object> returnValue = new HashMap<>();
         returnValue.put("byteCode", Base64.getEncoder().encodeToString(byteCode));
         returnValue.put("documentation", docsMap);
-        
+
         return returnValue;
     }
 }
